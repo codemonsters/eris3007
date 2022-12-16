4c9ae51f055438c15c21ef06e195bd6c1e073de0
yagueto <48566793+yagueto@users.noreply.github.com>
2022-12-16 20:05:02 +0100

    Por favor que no rompa nada

---
+++ src/hero.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var speed = 250
+
+func get_input():
+	# Detect up/down/left/right keystate and only move when pressed.
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
--- src/menu.gd
+++ src/menu.gd
@@ -5,12 +5,5 @@ extends Node2D
 func _ready():
 	$BackgroundColor/Button.grab_focus()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_button_pressed():
-	print("aqui2")
 	get_tree().change_scene_to_file("res://level.tscn")

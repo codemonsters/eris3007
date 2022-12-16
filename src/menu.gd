38f85908887062858ea6803f2370f09748e49d86
yagueto <48566793+yagueto@users.noreply.github.com>
2022-12-16 18:53:52 +0100

    Ahora puedes pulsar intro para arrancar el juego,mejorando la usabilidad en un 5000%

--- src/menu.gd
+++ src/menu.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("aqui")
+	$BackgroundColor/Button.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

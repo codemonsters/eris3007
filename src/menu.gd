a788b70d0e61e6d9a914732fff92a265f8d3bf4b
jorge-est <jorgeedecarrillo@gmail.com>
2023-03-31 20:09:43 +0200

    Añadido efecto parallax al menu (SIN ACABAR)

--- src/menu.gd
+++ src/menu.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$BackgroundColor/Button.grab_focus()
+	$Control/Button.grab_focus()
 
 func _on_button_pressed():
 	get_parent().start_game()
+
